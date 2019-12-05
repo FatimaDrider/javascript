1c5b42be4bf880b1ed8f67b76cb64c4997d5f465
FatimaDrider <driderf86@gmail.com>
2019-12-05 14:58:30 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,28 @@
+CTYPE html>
+<html lang="pt-BR">
+<head>
+	<meta charset="utf-8">fatima
+	
+	<link rel="stylesheet" type="text/css" href="">
+<title>Hello fatima</title>
+</head>
+<body>
+<div class="box">
+	<div class="board"></div>
+	<div class="stats"></div>
+</div>
+
+
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script><script type="text/javascript" src="js/test.js"></script>
+
+
+</body>
+</html>
+
+
+
+
+
+
+
+
