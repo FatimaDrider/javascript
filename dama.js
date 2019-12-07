f2d493f7df11e113c1a9d8b50077be63f817ff83
FatimaDrider <driderf86@gmail.com>
2019-12-07 23:47:06 +0100

    add

--- dama.js
+++ dama.js
@@ -1,5 +1,53 @@
 $(function(){
+var initialPosition ={
+ a8 :'A',
+ b8 :'A',
+ c8 :'A',
+ d8 :'A',
+ e8 :'A',
+ f8 :'A',
+ g8 :'A',
+ h8 :'A',
 
+ a7 :'A',
+ b7 :'A',
+ c7 :'A',
+ d7 :'A',
+ e7 :'A',
+ f7 :'A',
+ g7 :'A',
+ h7 :'A',
+
+ a2 :'B',
+ b2 :'B',
+ c2 :'B',
+ d2 :'B',
+ e2 :'B',
+ f2 :'B',
+ g2 :'B',
+ h2 :'B',
+
+ a1 :'B',
+ b1 :'B',
+ c1 :'B',
+ d1 :'B',
+ e1 :'B',
+ f1 :'B',
+ g1 :'B',
+ h1 :'B',
+
+};
+
+
+function newgame(){
+	$('.square-board').each(function(){
+		var square = $(this);
+		var sq = square.attr('id');
+		if (objsearchIndex(initialPosition,sq) !=null ) {
+			$(this).html('<div class="piece '+initialPosition[sq]+'"></div>')
+		}
+	});
+}
 function printplanche(){
 	var light = 1;
 	var colums =['a','b','c','d','e','f','g','h'];
@@ -7,8 +55,8 @@ function printplanche(){
 		for(var c=0;c < colums.length;++c)
 		{
 			var sq = colums[c]+l;
-			var lightdark =(light== 1)? 'light' : 'dark';
-		 $('.board').append('<div class="square-board '+lightdark+'" id="'+sq+'">'+sq+'</div>');
+			var lightdark =(light== 1) ? 'light' : 'dark';
+		 $('.board').append('<div class="square-board '+lightdark+'" id="'+sq+'"></div>');
 		light ^= 1;
 		}
 		light ^= 1;
@@ -16,34 +64,29 @@ function printplanche(){
 }
 
 printplanche();
+newgame();
 
 
-	function objseach(obj ,valeur){
+	function objSeach(obj ,valeur){
 		var retorno =null;
 		$.each(obj,function(i,val)
           {
           	if (val == valeur) {
-          		return i;
+          		retorno = i;
           	}
 
   			});
 		return retorno;
 	}
-
-
-
-function objseach(obj ,index){
+function objsearchIndex(obj ,index){
 		var retorno =null;
 		$.each(obj,function(i,val)
           {
           	if (i== index) {
-          		return i;
+          		retorno =i;
           	}
 
   			});
 		return retorno;
 	}
-
-
-
 	});
